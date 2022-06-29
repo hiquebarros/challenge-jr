0bdb70e1bd3e247496dfcc16beaa73fa0b00aaa1
Henrique Barros <henrique1_barros@hotmail.com>
2022-06-29 20:50:44 -0300

    feat: js files

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+const swiperBanner = new Swiper(".swiper-container", {
+  // Navigation arrows
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  });
