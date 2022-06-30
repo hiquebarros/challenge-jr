8021f83ecc0840c82ad5855f23c7de7b20eef25e
Henrique Barros <henrique1_barros@hotmail.com>
2022-06-29 23:43:07 -0300

    feat: repo configs

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-const swiperBanner = new Swiper(".swiper-container", {
+const swiperBanner = new Swiper(".banner", {
   // Navigation arrows
   
   navigation: {
@@ -6,8 +6,34 @@ const swiperBanner = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 
-  // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
-
   });
+
+  const swiperBrands = new Swiper(".brands", {
+    // Navigation arrows
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    slidesPerView: 1,
+
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 0,
+      },
+  
+    }});
